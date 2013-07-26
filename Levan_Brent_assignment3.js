a053b4b597f6058d74ae6b872a7ef5bfa0d88172
bnlevan <bnlevan@fullsail.edu>
2013-07-25 22:45:28 -0400

    initial commit

---
+++ Levan_Brent_assignment3.js
@@ -0,0 +1,11 @@
+//alert("JavaScript works!");
+
+//Brent Levan
+//Project 3
+//SDI 1307
+//Methods, Properties, and JSON
+
+var numberOfplayers =[1,2];
+
+console.log ("Please select number of players.");
+console.log ("number of players picked is" + numberOfplayers[1]);
